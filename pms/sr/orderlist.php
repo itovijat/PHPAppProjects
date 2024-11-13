3216834345c1bfb2ffc9ff735a5aad9e909b68f1
itovijat <it.ovijat@gmail.com>
2024-11-13 22:47:03 +0600

    total amount cal

--- pms/sr/orderlist.php
+++ pms/sr/orderlist.php
@@ -7,23 +7,81 @@
             <div class="card">
                 <div class="card-header noPrint">
                 <div class="row">
-                    <div class="col-12 col-md-6"> <h1 >Orders' List Range:</h1></div>
+                    <div class="col-12 col-md-12"> <h1 >Orders' List:</h1></div>
                     <form class="form-inline">
-                    <div class="col-5 col-md-3">
+
+                    
+                    <div class="col-6 col-md-1">
                     <input type="text" class="form-control " id="fromdate" name="fromdate" 
                     pattern='[0-9]{4}\.[0-9]{2}\.[0-9]{2}' title='Year.Month.Day' value="<?php if (isset($_GET['fromdate']) )
                       {echo $_GET['fromdate'];}
                        else {echo date('Y.m.d');} 
                        ?>" required>
                     </div>
-                    <div class="col-5 col-md-3">
+                    <div class="col-6 col-md-1">
                         <input type="text" class="form-control" id="todate" name="todate"
                          pattern='[0-9]{4}\.[0-9]{2}\.[0-9]{2}' title='Year.Month.Day' value="<?php if (isset($_GET['todate']) )
                       {echo $_GET['todate'];}
                        else {echo date('Y.m.d');} 
                        ?>" required>
                     </div>
-                    <div class="col-2 col-md-2">                            <button type="submit" class="btn btn-primary">Search</button>
+
+
+                    <div class="col-6 col-md-1">
+                        <input type="text" class="form-control" id="shop" name="shop"
+                         <?php if (isset($_GET['shop']) && $_GET['shop'] != null)
+                      {echo "value='".$_GET['shop']."'";}
+                       else {echo "placeholder='all shop/phone'";} 
+                       ?> >
+                    </div>
+
+                    <div class="col-6 col-md-1">
+                        <input type="text" class="form-control" id="route" name="route"
+                         <?php if (isset($_GET['route']) && $_GET['route'] != null)
+                      {echo "value='".$_GET['route']."'";}
+                       else {echo "placeholder='all route'";} 
+                       ?> >
+                    </div>
+
+                    <div class="col-6 col-md-1">
+                        <input type="text" class="form-control" id="memo" name="memo"
+                         <?php if (isset($_GET['memo']) && $_GET['memo'] != null)
+                      {echo "value='".$_GET['memo']."'";}
+                       else {echo "placeholder='all memo'";} 
+                       ?> >
+                    </div>
+
+                    <div class="col-6 col-md-1">
+                        <input type="text" class="form-control" id="product" name="product"
+                         <?php if (isset($_GET['product']) && $_GET['product'] != null)
+                      {echo "value='".$_GET['product']."'";}
+                       else {echo "placeholder='all product'";} 
+                       ?> >
+                    </div>
+
+                    <div class="col-6 col-md-1">
+                        <input type="number" class="form-control" id="id" name="id"
+                         <?php if (isset($_GET['id']) && $_GET['id'] != null)
+                      {echo "value='".$_GET['id']."'";}
+                       else {echo "placeholder='all id'";} 
+                       ?> >
+                    </div>
+
+                    <div class="col-6 col-md-1">
+                        <select class="form-control" id="status" name="status">
+                            <option value="" <?= !isset($_GET['status']) || $_GET['status'] === null ? 'selected' : ''; ?>>all status</option>
+                            <option value="0" <?= isset($_GET['status']) && $_GET['status'] == '0' ? 'selected' : ''; ?>>Pending</option>
+                            <option value="1" <?= isset($_GET['status']) && $_GET['status'] == '1' ? 'selected' : ''; ?>>Accepted</option>
+                          
+                            <option value="3" <?= isset($_GET['status']) && $_GET['status'] == '3' ? 'selected' : ''; ?>>Delivered</option>
+                            <option value="4" <?= isset($_GET['status']) && $_GET['status'] == '4' ? 'selected' : ''; ?>>Rejected</option>
+                            <option value="5" <?= isset($_GET['status']) && $_GET['status'] == '5' ? 'selected' : ''; ?>>Returned</option>
+
+                        </select>
+                    </div>
+
+
+                    <div class="col-12 mx-2 my-2 col-md-1 text-center">                            <button type="submit" class="btn btn-primary">Search</button>
                     </div>
                     </form>
 
@@ -63,7 +121,42 @@ echo "<p style='text-align: center;'>Order List From: <b>".$fromdate."</b> To: <
 $todate."</b> For : ".$_SESSION['email']."</p>";
 $sql = "SELECT * FROM visit WHERE mo='" . $_SESSION['email'] .
 "' AND company='" . $_SESSION['company'] . "' AND reason='order' AND status != 2 AND odate BETWEEN '".
-$fromdate."' AND '".$todate."' ORDER BY SN DESC";
+$fromdate."' AND '".$todate."'";
+
+
+if (isset($_GET['memo']) && $_GET['memo'] != '') {
+    $sql .= " AND memo LIKE '%".$_GET['memo']."%'";
+}
+if (isset($_GET['shop']) && $_GET['shop'] != '') {
+    $sql .= " AND (shop LIKE '%".$_GET['shop']."%' OR phone LIKE '%".$_GET['shop']."%')";
+}
+if (isset($_GET['phone']) && $_GET['phone'] != '') {
+    $sql .= " AND phone LIKE '%".$_GET['phone']."%'";
+
+}
+if (isset($_GET['route']) && $_GET['route'] != '') {
+    $sql .= " AND route LIKE '%".$_GET['route']."%'";
+}
+
+if (isset($_GET['id']) && $_GET['id'] != '') {
+    $sql .= " AND SN='" . $_GET['id'] . "'";
+    
+}
+if (isset($_GET['status']) && $_GET['status'] != '') {
+    $sql .= " AND status='" . $_GET['status'] . "'";
+}
+
+
+$sql .= " ORDER BY SN DESC";
+
+// echo "<p style='text-align: center; font-size: 1.5em; color: red'>SQL: ".$sql."</p>";
+    
+
+
+
+
+
+
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
@@ -71,22 +164,18 @@ if (mysqli_num_rows($result) > 0) {
     echo "<thead>";
     echo "<tr>";
     echo "<th class='noPrint'>Action</th>";
-
-  
     echo "<th>Details</th>";
     echo "<th>Orders</th>";
-
-
-
-    
-
-   
-   
-
     echo "</tr>";
     echo "</thead>";
     echo "<tbody>";
 $count=1;
+$totalpending=0.0;
+$totalaccepted=0.0;
+$totalrejected=0.0;
+$totalreturned=0.0;
+$totaldelivered=0.0;
+$totalamount=0.0;
     while ($row = mysqli_fetch_assoc($result)) {
         echo "<tr>";
         if($row['status'] == 0){
@@ -109,14 +198,26 @@ $count=1;
 
 echo "<td>";
         $orderSql = "SELECT * FROM orders WHERE snvisit='" . $row['SN'] . "'";
+
+        if (isset($_GET['product']) && $_GET['product'] != '') {
+            $orderSql = "SELECT * FROM orders WHERE snvisit='" . $row['SN'] . "' AND pn LIKE '%".$_GET['product']."%'";
+        }
         $orderResult = mysqli_query($conn, $orderSql);
 
         if (mysqli_num_rows($orderResult) > 0) {
+           
+
             $total=0.0;
             while ($orderRow = mysqli_fetch_assoc($orderResult)) {
                 echo "<div style='border: 1px solid #ccc'>".$orderRow['pn'] . " (<i>"
                  . $orderRow['unit'] ."</i>) ". $orderRow['quantity'] . "@" . $orderRow['rate'] .
                   "=" . ($orderRow['rate'] * $orderRow['quantity'])."/=</div>";
+
+
+
+
+
+
 
                 $total += $orderRow['rate'] * $orderRow['quantity'];
             }
@@ -124,6 +225,25 @@ echo "<td>";
             echo "No orders found";
         }
         echo "<div>Total: " . number_format($total, 2) . "/= <i style='font-size: 12px'>".$row['comment']."</i></div>";
+        $totalamount=$totalamount+$total;
+
+        if($row['status'] == 0){
+            $totalpending=$totalpending+$total;  
+           
+        }else if($row['status'] == 1){
+            $totalaccepted=$totalaccepted+$total;  
+
+        }else if($row['status'] == 3){
+            $totaldelivered=$totaldelivered+$total;  
+
+        }else if($row['status'] == 4){
+            $totalrejected=$totalrejected+$total;  
+
+        }
+        elseif ($row['status'] == 5){
+            $totalreturned=$totalreturned+$total;  
+
+        }
 
         echo "</td>";
 
@@ -140,6 +260,14 @@ echo "<td>";
     echo "<p style='text-align: center; font-size: 2em; color: red'>0 results</p>";
 }
 ?>
+
+<p>Total Pending Amount=<?= $totalpending?>/=</p>
+<p>Total Accepted Amount=<?= $totalaccepted?>/=</p>
+<p>Total Delivered Amount=<?= $totaldelivered?>/=</p>
+<p>Total Delivered Amount=<?= $totalrejected?>/=</p>
+<p>Total Returned Amount=<?= $totalreturned?>/=</p>
+
+<p>Total Amount=<?= $totalamount?>/=</p>
 
 
 
